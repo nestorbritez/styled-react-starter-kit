29d75f71ecdc8040360d98f4baac0e80d201ecd8
Nestor Britez <nestor@britez.me>
2021-07-31 19:42:57 -0300

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,85 @@
 import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import tw from 'tailwind-styled-components'
 
-function App() {
+import logo from './images/favicon.svg'
+
+const Container = tw.div`
+  max-w-lg h-screen
+  flex flex-col items-center justify-center space-y-5
+  m-auto
+  text-center
+`
+
+const Logo = tw.img`
+  max-w-xs
+`
+
+const Button = tw.button`
+  bg-purple-500
+  py-2 px-4
+  rounded
+`
+
+const Counter = tw.span`
+  ml-5 p-3
+  bg-pink-600
+  rounded-full
+`
+
+const Code = tw.code`
+  py-1 px-2 mx-2
+  bg-yellow-400
+  text-black
+  rounded
+`
+
+const Link = tw.a`
+  border-b border-purple-400
+  mx-2
+`
+
+const App: React.FC = () => {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <Container>
+      <Logo src={logo} alt="logo" />
+      <p>Hello Vite React App with Styled Taildwind!</p>
+      <p>
+        <Button type="button" onClick={() => setCount((count) => count + 1)}>
+          Counter state is →
+        </Button>
+        <Counter>{count}</Counter>
+      </p>
+      <p>
+        Edit <Code>App.tsx</Code> and save to test HMR updates.
+      </p>
+      <p>
+        <Link
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </Link>
+        {' | '}
+        <Link
+          href="https://vitejs.dev/guide/features.html"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Vite Docs
+        </Link>{' '}
+        {' | '}
+        <Link
+          href="https://tailwindcss.com/docs"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Tailwindcss Docs
+        </Link>
+      </p>
+    </Container>
   )
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+module.exports = {
+  mode: 'jit',
+  purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {},
+  variants: {},
+  plugins: [],
+}
